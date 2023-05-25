a96e672066f6c94cea85ba536d0053e60b27128f
EuryJesus <Eury0218@hotmail.com>
2023-05-24 20:33:18 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+ function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+  return response.json();
+  })
+  .then((books) => {
+  renderBooks(books);
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  }
+  function renderBooks(books) {
+    books.forEach(book => {
+    renderBook(book)
+    });
+    }
+    function renderBook(book) {
+      const unorderedList = document.getElementById("list");
+      const li = document.createElement("li");
+      li.textContent = book.title
+      li.addEventListener("click", () => showBookCard(book))
+      unorderedList.appendChild(li)
+      }
+      
